2527d3616b2d91fa31c897208f5155f5f3ca64ae
Mallhw <67278784+Mallhw@users.noreply.github.com>
2024-12-13 11:53:15 -0500

    Add files via upload

---
+++ Codeforces/Practice/MathematicalAddition.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <streambuf>
+#include <sstream>
+#include <unordered_set>
+#include <bitset>
+
+using namespace std;
+typedef long long ll;
+
+#define endll '\n'
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int t;
+    cin >> t;
+    while(t--) {
+        
+    }
+    
+    return 0;
+}
