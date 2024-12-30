1ff222f40163b8a7dd9bf670cec5d41972faac10
mallhw <Matthewli150@gmail.com>
2024-12-30 10:08:00 -0500

    GoodBye 2024

---
+++ Practice/TenderCarpenter.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+#define max(a, b) (a < b ? b : a) 
+#define min(a, b) ((a > b) ? b : a) 
+#define dbg(x) cerr << __LINE__ << ": " << #x << "=" << x << endl;
+#define mod 1000000007 
+#define FOR(a, c) for (int(a) = 0; (a) < (c); (a)++) 
+#define FORL(a, b, c) for (int(a) = (b); (a) <= (c); (a)++) 
+#define FORR(a, b, c) for (int(a) = (b); (a) >= (c); (a)--) 
+#define INF 1000000000000000003 
+typedef long long int ll; 
+typedef vector<long long> vll;
+typedef vector<int> vi; 
+typedef pair<int, int> pi; 
+#define F first 
+#define S second 
+#define PB push_back 
+#define POB pop_back 
+#define MP make_pair 
+void solve() {
+    int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        for(int i = 1; i < n; i++) {
+            ll max = max(a[i-1], a[i]);
+            ll min = min(a[i-1], a[i]);
+            if(min * 2 > max) {
+                cout << "YES" << endl;
+                return;
+            }
+        }
+        cout << "NO" << endl;
+        return;
+}
+int main() 
+{ 
+    ios::sync_with_stdio(0); 
+    cin.tie(0); 
+    int T; 
+    cin >> T; 
+    while (T--) { 
+        solve();
+    } 
+    return 0; 
+} 
